60ff56fa229cd92a2f3dca2467c1e596c88d2086
rattawut jumprom <rattawut@hotmail.com>
2020-03-06 18:31:56 +0700

    update structure

--- back-end/src/file/file.controller.ts
+++ back-end/src/file/file.controller.ts
@@ -1,29 +1,11 @@
 import {
   Controller,
   Post,
-  HttpStatus,
   UseInterceptors,
   UploadedFile
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { FileService } from "./file.service";
-const { Storage } = require("@google-cloud/storage");
-import * as path from "path";
-import * as fs from "fs";
-
-const gc = new Storage({
-  keyFilename: path.join(
-    __dirname,
-    "../my-project-2018-206114-07270400a695.json"
-  ),
-  projectId: "my-project-2018-206114"
-});
-
-const coolFilesBucket = gc.bucket("bucket_1_got");
-
-const getPublicUrl = fileName => {
-  return `https://storage.googleapis.com/bucket_1_got/${fileName}`;
-};
 
 @Controller()
 export class FileController {
@@ -31,27 +13,7 @@ export class FileController {
 
   @Post("files")
   @UseInterceptors(FileInterceptor("file"))
-  async uploadFile(@UploadedFile() file) {
-    const { originalname, path, mimetype } = file;
-    return await new Promise((resolve, reject) =>
-      fs
-        .createReadStream(path)
-        .pipe(
-          coolFilesBucket.file(originalname).createWriteStream({
-            metadata: {
-              contentType: mimetype
-            },
-            resumable: false
-          })
-        )
-        .on("error", error => {
-          reject({ success: false, error });
-        })
-        .on("finish", async () => {
-          await coolFilesBucket.makePublic();
-          const _path = getPublicUrl(originalname);
-          resolve(_path);
-        })
-    );
+  uploadFile(@UploadedFile() file) {
+    return this.fileService.uploadToGCP(file);
   }
 }
--- back-end/src/file/file.service.ts
+++ back-end/src/file/file.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from "@nestjs/common";
+const { Storage } = require("@google-cloud/storage");
+
+import * as del from "del";
+import * as path from "path";
+import * as fs from "fs";
+
+const gc = new Storage({
+  keyFilename: path.join(
+    __dirname,
+    "../my-project-2018-206114-07270400a695.json"
+  ),
+  projectId: "my-project-2018-206114"
+});
+const coolFilesBucket = gc.bucket("bucket_1_got");
 
 @Injectable()
 export class FileService {
-  public generateJWT(user) {
+  public uploadToGCP = async file => {
+    const { filename, path, mimetype } = file;
+    return await new Promise((resolve, reject) =>
+      fs
+        .createReadStream(path)
+        .pipe(
+          coolFilesBucket.file(filename).createWriteStream({
+            metadata: {
+              contentType: mimetype
+            },
+            resumable: false
+          })
+        )
+        .on("error", error => {
+          reject({ success: false, error });
+        })
+        .on("finish", async () => {
+          await coolFilesBucket.makePublic();
+          const _path = this.getPublicUrl(filename);
+          resolve(_path);
+        })
+    );
+  };
+
+  private getPublicUrl = fileName => {
+    return `https://storage.googleapis.com/bucket_1_got/${fileName}`;
+  };
+
+  public deleteFile = async path => {
+    await del([path], { force: true });
     return true;
-  }
+  };
 }
